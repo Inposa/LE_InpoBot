ae99b9cefa74e04a81b3204bbcbd39fabd5e6a9c
Inposa <donhippo04@gmail.com>
2019-06-04 15:15:13 +0200

    Premiers test de la commande cat avec le module request plutôt que le module https

---
+++ commands/catRequest.js
@@ -0,0 +1,33 @@
+/* const https = require('https');
+exports.run = (client, message)=>{
+	const url = 'https://aws.random.cat/meow';
+
+	https.get(url, function(res) {
+		let body = '';
+
+		res.on('data', function(chunk) {
+			body += chunk;
+		});
+
+		res.on('end', function() {
+			const response = JSON.parse(body);
+
+			message.channel.send(`${message.author} Voici un petit chat ! `, { file: response.file });
+
+		});
+	}).on('error', function(e) {
+		console.log('Une erreur : ', e);
+	});
+};*/
+
+const request = require('request');
+
+exports.run = (client, message)=>{
+	const url = 'https://aws.random.cat/meow';
+	request(url, function(error, response, body) {
+		message.send('Ceci est un test de recherche de chat avec le module request !');
+		message.send(`Erreur: ${error}`);
+		message.send(`Réponse: ${response}`);
+		message.send(`Body: ${body}`);
+	});
+};
