f2720039139ad544a1021bc98ab194bea85bc308
Inposa <donhippo04@gmail.com>
2019-06-08 20:54:57 +0200

    Correction de bug avec endswith()

--- commands/reponse_auto.js
+++ commands/reponse_auto.js
@@ -3,17 +3,17 @@ const config = require('../config.json');
 exports.run = (client, message)=>{
 
 	// Mettre le regex dans le fichier config
-	const verif = message.replace(config.regex, '').trim();
+	const verif = message.replace(config.regex, '').trim().toLowerCase();
 
 	// Procédure à faire --> utiliser une enmap contenant chaque mot clé à détecter,
 	// chaque entrée accompagnée de sa réponse perso (peut-être qu'il faudra la
 	// parcourir de long en large et de travers)
 
-	if(verif.endWith('quoi')) {
+	if(verif.endsWith('quoi')) {
 		message.channel.send('feur');
 	}
 
-	else if(verif.endWith('oui')) {
+	else if(verif.endsWith('oui')) {
 		message.channel.send('stiti');
 	}
 
